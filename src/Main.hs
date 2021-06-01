e5ee9aefae9c2ad1c70b1b0ffb00228cee716669
Josiah Baldwin <12803156+MisterEggnog@users.noreply.github.com>
2021-06-01 12:25:22 -0700

    Moved matchString to switchs

--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,16 @@ module Main where
 main :: IO ()
 main = undefined
 
+newtype Switchs = Switchs {
+   matchString :: Option String -- This should have a space in it, this is not checked
+}
+
 -- First/Last names should be the file string, word splits will be added.
--- Shuffle name should have a space in it, this is not checked.
+-- If switch.matchString is Just, then the returned list will only be those that have the same characters as matchString.
+-- This is technically the default behaviour
 namegener ::
   String ->  -- First names
   String ->  -- Last names
-  String ->  -- Shuffle name
   Switchs -> -- Command switchs
   [String]
 namegener = undefined
