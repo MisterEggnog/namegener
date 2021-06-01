2846f437a3f5cdaad56e08e904cf7a4111706c41
Josiah Baldwin <12803156+MisterEggnog@users.noreply.github.com>
2021-06-01 12:12:38 -0700

    Namegener function sig in Main.hs
    Hopefully I will only need monads in Main

--- src/Main.hs
+++ src/Main.hs
@@ -2,3 +2,13 @@ module Main where
 
 main :: IO ()
 main = undefined
+
+-- First/Last names should be the file string, word splits will be added.
+-- Shuffle name should have a space in it, this is not checked.
+namegener ::
+  String ->  -- First names
+  String ->  -- Last names
+  String ->  -- Shuffle name
+  Switchs -> -- Command switchs
+  [String]
+namegener = undefined
