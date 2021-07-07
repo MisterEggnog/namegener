4b671b1729e03a9716981a6206318e0f4aa576f0
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2021-07-07 15:06:34 -0700

    Setup test framework & mergeString test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,14 @@
 
+import Data.Set (Set, fromList)
+import Ananamer
 import Test.HUnit
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = runTestTT tests >>= (putStrLn . showCounts)
 
+tests = TestList [TestLabel "mergeStrings" mergeStringsTest]
+
+mergeStringsTest = TestCase (assertEqual "Merge string produces all possible merges" expected given)
+  where expected = fromList ["Mark Markson", "Mark Johnson", "Mark Dinkson", "John Markson", "John Johnson", "John Dinkson", "Dink Markson", "Dink Johnson", "Dink Dinkson"]
+        given = fromList $
+          Ananamer.mergeStrings ["Mark", "John", "Dink"] ["Markson", "Johnson", "Dinkson"]
