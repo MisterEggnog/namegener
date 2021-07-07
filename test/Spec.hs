73d4846119500e03c149c3afd856c40115c3eab8
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2021-07-07 15:19:09 -0700

    Basic test for splitNamesList
    Does not currently handle shuffling the names, nor do I know how to do
    that.

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,9 +6,13 @@ import Test.HUnit
 main :: IO ()
 main = runTestTT tests >>= (putStrLn . showCounts)
 
-tests = TestList [TestLabel "mergeStrings" mergeStringsTest]
+tests = TestList [TestLabel "mergeStrings" mergeStringsTest, TestLabel "splitNames splits" splitNamesSplits]
 
 mergeStringsTest = TestCase (assertEqual "Merge string produces all possible merges" expected given)
   where expected = fromList ["Mark Markson", "Mark Johnson", "Mark Dinkson", "John Markson", "John Johnson", "John Dinkson", "Dink Markson", "Dink Johnson", "Dink Dinkson"]
         given = fromList $
           Ananamer.mergeStrings ["Mark", "John", "Dink"] ["Markson", "Johnson", "Dinkson"]
+
+splitNamesSplits = TestCase (assertEqual "splitNames splits according to newlines in string, no change in order" expected given)
+  where expected = ["john", "mark", "dink"]
+        given    = Ananamer.splitNameLists False "john\nmark\ndink\n"
